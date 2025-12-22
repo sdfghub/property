e7802219069a8e2973d3c29c558d9cc6c42d0a54
sdfghub <bogdan.boji@gmail.com>
2025-12-22 16:40:40 +0200

    separate cd actions

---
+++ infra/cdk/lib/access-stack.ts
@@ -0,0 +1,50 @@
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import {
+  Instance,
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  MachineImage,
+  SecurityGroup,
+  Vpc,
+  SubnetType,
+  Port,
+} from 'aws-cdk-lib/aws-ec2'
+import { ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
+
+interface Props extends cdk.StackProps {
+  vpc: Vpc
+  dbSecurityGroup: SecurityGroup
+}
+
+export class AccessStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: Props) {
+    super(scope, id, props)
+
+    const bastionSg = new SecurityGroup(this, 'BastionSg', { vpc: props.vpc })
+    bastionSg.addEgressRule(props.dbSecurityGroup, Port.tcp(5432))
+    props.dbSecurityGroup.addIngressRule(bastionSg, Port.tcp(5432))
+
+    const bastionRole = new Role(this, 'BastionRole', {
+      assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')],
+    })
+
+    const bastion = new Instance(this, 'Bastion', {
+      vpc: props.vpc,
+      vpcSubnets: { subnetType: SubnetType.PUBLIC },
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.NANO),
+      machineImage: MachineImage.latestAmazonLinux2023(),
+      securityGroup: bastionSg,
+      role: bastionRole,
+    })
+
+    new cdk.CfnOutput(this, 'BastionInstanceId', {
+      value: bastion.instanceId,
+    })
+    new cdk.CfnOutput(this, 'BastionSecurityGroupId', {
+      value: bastionSg.securityGroupId,
+    })
+  }
+}
--- infra/cdk/lib/data-stack.ts
+++ infra/cdk/lib/data-stack.ts
@@ -8,37 +8,17 @@ import {
   Vpc,
   SubnetType,
   SecurityGroup,
-  Peer,
-  Port,
-  Instance,
-  MachineImage,
 } from 'aws-cdk-lib/aws-ec2'
-import { Role, ServicePrincipal, ManagedPolicy } from 'aws-cdk-lib/aws-iam'
 
 interface Props extends cdk.StackProps { vpc: Vpc }
 
 export class DataStack extends cdk.Stack {
   public readonly db: DatabaseInstance
+  public readonly dbSecurityGroup: SecurityGroup
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props)
-    const bastionSg = new SecurityGroup(this, 'BastionSg', { vpc: props.vpc })
     const dbSg = new SecurityGroup(this, 'DbSg', { vpc: props.vpc })
-    dbSg.addIngressRule(bastionSg, Port.tcp(5432))
-    bastionSg.addEgressRule(Peer.anyIpv4(), Port.tcp(5432))
-
-    const bastionRole = new Role(this, 'BastionRole', {
-      assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
-      managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')],
-    })
-
-    const bastion = new Instance(this, 'Bastion', {
-      vpc: props.vpc,
-      vpcSubnets: { subnetType: SubnetType.PUBLIC },
-      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.NANO),
-      machineImage: MachineImage.latestAmazonLinux2023(),
-      securityGroup: bastionSg,
-      role: bastionRole,
-    })
+    this.dbSecurityGroup = dbSg
     this.db = new DatabaseInstance(this, 'Postgres', {
       vpc: props.vpc,
       engine: DatabaseInstanceEngine.postgres({ version: PostgresEngineVersion.VER_16 }),
@@ -52,12 +32,8 @@ export class DataStack extends cdk.Stack {
       publiclyAccessible: false,
       credentials: { username: 'postgres' }
     })
-
-    new cdk.CfnOutput(this, 'BastionInstanceId', {
-      value: bastion.instanceId,
-    })
-    new cdk.CfnOutput(this, 'BastionSecurityGroupId', {
-      value: bastionSg.securityGroupId,
+    new cdk.CfnOutput(this, 'DbSecurityGroupId', {
+      value: dbSg.securityGroupId,
     })
   }
 }
